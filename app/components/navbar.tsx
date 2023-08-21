4bf9b31a298b7356a0b260f8907bc0c2bcac831b
Jose Ale <jrodriguezp265@gmail.com>
2023-08-20 23:17:25 -0400

    navbar added

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -15,12 +15,9 @@ const Navbar = async() => {
     <div className='border-b'>
         <Container>
             <div className=' relative px-4 sm:px-6 lg:px-8 h-16 flex items-center'>
-                <Link href='/' className=' ml-4 flex lg:ml-0 gap-x-2'>
-                    <p className='font-bold text-xl'>IMPRENTADOS CL</p>
-                </Link>
-
+               
                 <MainNav data={stores}/>
-                 <NavbarActions /> 
+               
             </div>
         </Container>
         
